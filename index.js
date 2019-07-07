b502b4d03df4f059591102d8d18b77a1e19408d7
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-07 17:43:47 -0400

    used filter iterator

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  let matches = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+  return matches
+}
+
+function fuzzyMatch(drivers, string) {
+  let matches = drivers.filter(driver => driver.charAt(0).toLowerCase() === string.charAt(0).toLowerCase())
+  return matches
+}
+
+function matchName(drivers, string) {
+  let matches = drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase())
+  return matches
+}
